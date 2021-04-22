79345c918356b4595a7f8d71a877fe6e72a51088
arangananayakkara <arangananayakkara@fintrix.com.au>
2021-04-22 19:39:11 +1000

    v1

--- src/main/java/com/cosight/app/App.java
+++ src/main/java/com/cosight/app/App.java
@@ -2,24 +2,27 @@ package com.cosight.app;
 
 
 
+
 import au.com.cosight.sdk.EcsTaskInfo;
-import au.com.cosight.sdk.plugin.core.ICosightExecutionContext;
-import au.com.cosight.sdk.plugin.core.drive.CosightDrive;
-import au.com.cosight.sdk.plugin.core.drive.CosightDriveManager;
+import au.com.cosight.sdk.annotation.EnableCosightDrive;
+import au.com.cosight.sdk.annotation.EnableCosightRuntimeContext;
+import au.com.cosight.sdk.plugin.drive.CosightDrive;
+import au.com.cosight.sdk.plugin.drive.CosightDriveManager;
+import au.com.cosight.sdk.plugin.runtime.ICosightExecutionContext;
 import com.amazonaws.services.s3.model.*;
 
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.io.ClassPathResource;
+
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.util.ResourceUtils;
+
 
 
 import java.io.File;
@@ -29,6 +32,8 @@ import java.io.InputStream;
  * spring boot command line sample application
  */
 @SpringBootApplication
+@EnableCosightRuntimeContext
+@EnableCosightDrive
 public class App implements CommandLineRunner  {
 
 	private static Logger logger = LoggerFactory
@@ -67,10 +72,11 @@ public class App implements CommandLineRunner  {
 		File f = new File("/tmp/test.csv"); // has 20G local temp storage
 		FileUtils.copyInputStreamToFile(input,f);
 		logger.info("File {}, copy to local success: {},size {} byte,time stamp {} ",f.getAbsolutePath(),f.exists(),f.length(),f.lastModified());
-		logger.info("plugin context runtime parameter map {}", cosightExecutionContext.getParameters());
+		logger.info("plugin context  {}", cosightExecutionContext.getParameters());
 
-		String out =(String) cosightExecutionContext.getParameters().get("output-folder");
+		String out = (String) cosightExecutionContext.getParameters().get("output-folder");
 		logger.info("Uploading to {}",out+"/upload-test.csv");
+
 		CosightDrive drive = driveManager.driveInstance();
 				boolean success = drive.copyLocal(f,out+"/upload-test.csv");
 		logger.info(" upload {}",success);
@@ -80,12 +86,9 @@ public class App implements CommandLineRunner  {
 		if (object != null) {
 			logger.info("download successful s3://{}/{}",object.getBucketName(),object.getKey());
 		}
-		;
-
 		logger.info("Uploading file /system/test-plugin/test.csv ( THIS SHOULD FAIL)");
 		success = drive.copyLocal(f,"/system/test-plugin/test.csv");
 		logger.info("Upload success {}",success);
-
 		logger.info("==== END PLUGIN RUN ===");
 
 	}
